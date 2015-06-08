cc0532d362e490b5bc46a4f8ee61db05ac1f01e4
luckygarnet <jirapa.garnet@gmail.com>
2015-06-08 15:26:39 +0700

    12

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -11,10 +11,10 @@
         <link rel="stylesheet" href="<?php echo base_url('assets/font-awesome/css/font-awesome.css');?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css');?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/css/style-responsive.css');?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/themes/toggles.css')?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/themes/toggles-modern.css')?>">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/bootstrap-datepicker/css/datepicker.css')?>" />
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/bootstrap-daterangepicker/daterangepicker.css')?>" />
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/themes/toggles.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/themes/toggles-modern.css');?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/bootstrap-datepicker/css/datepicker.css');?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/js/bootstrap-daterangepicker/daterangepicker.css');?>" />
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
